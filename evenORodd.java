5156e5e32dd4522cdcf6896a15aaf0caabb27303
sultannalmutiri <105142287+sultannalmutiri@users.noreply.github.com>
2024-03-05 23:16:31 +0300

    Create evenORodd.java

---
+++ evenORodd.java
@@ -0,0 +1,24 @@
+package evenORodd.java;
+  import java.util.Scanner;
+
+
+public class EvenORodd{
+
+public static void main (String [] args){
+
+Scanner i = new Scanner (System.in);
+  int k = i.nextInt();
+  if (k % 2 == 0){
+
+    System.out.println("even number");
+  }else{
+
+    System.out.println("odd number");
+  }
+  
+
+
+
+
+} 
+}
